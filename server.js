48c59b3a85d9febed0b2279f077fb2325d70305f
YW Z <32604270+echoztoronto@users.noreply.github.com>
2021-03-26 18:53:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+const path = require('path');
 app.use(express.static(path.join(__dirname, '/pub')))
 
 // Mongo and Mongoose
