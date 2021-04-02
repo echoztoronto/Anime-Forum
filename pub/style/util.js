01f50a50e3a9094e97f4fbd06b9c203b188af092
Ziang Zhang <49383311+Ziang-Zhang@users.noreply.github.com>
2021-04-02 05:57:30 -0400

    edit util.js add set up cookie; get cookie; logout

--- pub/style/util.js
+++ pub/style/util.js
@@ -75,22 +75,31 @@ function calculate_exp_and_level(level, exp) {
     return [updated_level, exp];
 }
 
-function getCookie(cname) {  //from https://www.w3schools.com/js/js_cookies.asp
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+// setup cookie
+// cookie name, cookie value,  cookie expire day
+function setCookie (cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
-function log_out() {
-    document.cookie = "username= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+// get cookie
+function getCookie (cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) {
+            //("username=John Doe").substring("username=".length);
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
+
+function log_out () {
+    setCookie("username", null);
+    // document.cookie = null;
     console.log(document.cookie);
 }
