0d20abda1553d05e899cea21f5d7b814b73d2fb3
Songheng <songheng.yin@mail.utoronto.ca>
2021-04-06 14:51:30 +0800

    Ranking.js Done, refactor, fix bug, improve function structure

--- pub/style/ranking.js
+++ pub/style/ranking.js
@@ -7,35 +7,32 @@ const table_right = document.querySelector('#right_table');
 async function show_users_most_answers(e){
     e.preventDefault();
     const res = await fetch('/allUsers');       // GET all users
-    const user_list = await res.json();
+    let user_list = await res.json();
 
-    user_list.sort(function(a, b){
-        return b.num_answers - a.num_answers;
-    });
-    let i = 0;
-    for (i = 0; i < user_list.length; i++){    // ONLY display users have answered questions
-        if (user_list[i].num_answers == 0){
-            break;
-        }
-    }
-    update_ranking_page(user_list.slice(0, i), table_left, 'left');
+    user_list = user_list.filter(user => user.num_answers > 0);
+    user_list = user_list.map(user => ({
+        "userID": user.userID,
+        "displayName": user.displayName,
+        "level": user.level   
+    }));
+    user_list.sort((a, b) => b.content - a.content);
+    update_ranking_page(user_list, table_left, 'left');
 }
 
 async function show_users_most_accepted(e){
     e.preventDefault();
     const res = await fetch('/allUsers');       // GET all users
-    const user_list = await res.json();
+    let user_list = await res.json();
 
-    user_list.sort(function(a, b){
-        return (b.num_accepted / b.num_answers) - (a.num_accepted / a.num_answers);
-    });
-    let i = 0;
-    for (i = 0; i < user_list.length; i++){    // ONLY display users have answered questions
-        if (user_list[i].num_accepted == 0){
-            break;
-        }
-    }
-    update_ranking_page(user_list.slice(0, i), table_right, 'right');
+    user_list = user_list.filter(user => user.num_answers > 0 && user.num_accepted > 0);
+    user_list = user_list.map(user => ({
+        "content": ((user.num_accepted / user.num_answers) * 100).toFixed(2) + '%',
+        "userID": user.userID,
+        "displayName": user.displayName,
+        "level": user.level   
+    }));
+    user_list.sort((a, b) => b.content - a.content);
+    update_ranking_page(user_list, table_right, 'right');
 }
 
 function update_ranking_page(users, table, flag){
@@ -44,11 +41,7 @@ function update_ranking_page(users, table, flag){
         let cell0 = row.insertCell(0);
         let cell1 = row.insertCell(1);
         let cell2 = row.insertCell(2);
-        if (flag == 'right'){      // flag is the indicator for left/right table
-            cell0.innerHTML = `${users[i].num_accepted / users[i].num_answers}`;
-        }else{
-            cell0.innerHTML = `${users[i].num_answers}`;
-        }
+        cell0.innerHTML = `${users[i].content}`;
         cell1.innerHTML = `<a target="_blank" href="profile.html#${users[i].userID}">${users[i].displayName}</a>`;
         cell2.innerHTML = `Lv.${users[i].level}`;
         if (i % 2){
